385c827661448b4b0a0d9edfea65c9a354aa97c0
MrBotBoy <zebotboy@gmail.com>
2024-12-24 23:34:50 -1000

    First Commit

--- ChassisPractice2025/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ ChassisPractice2025/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -38,6 +38,7 @@ public class ExampleSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    System.out.print("hi");
   }
 
   @Override
